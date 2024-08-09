8a43596c5f96477efdcfa57f63e7e94a7bf90d7d
Jessica Sturgell <sturgell01@gmail.com>
2024-08-09 11:10:54 -0400

    initial commit

---
+++ Roommates/Models/Roommate.cs
@@ -0,0 +1,13 @@
+﻿namespace Roommates.Models
+{
+    // C# representation of the Roommate table
+    public class Roommate
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int RentPortion { get; set; }
+        public DateTime MovedInDate { get; set; }
+        public Room Room { get; set; }
+    }
+}
---
+++ Roommates/Repositories/ChoreRepository.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.Data.SqlClient;
+using Roommates.Models;
+using System.Collections.Generic;
+
+namespace Roommates.Repositories
+{
+    /// <summary>
+    ///  This class is responsible for interacting with Chore data.
+    ///  It inherits from the BaseRepository class so that it can use the BaseRepository's Connection property
+    /// </summary>
+    public class ChoreRepository : BaseRepository
+    {
+        /// <summary>
+        ///  When new ChoreRepository is instantiated, pass the connection string along to the BaseRepository
+        /// </summary>
+        public ChoreRepository(string connectionString) : base(connectionString) { }
+
+        // ...We'll add some methods shortly...
+        /// <summary>
+        ///  Get a list of all Chores in the database
+        /// </summary>
+        public List<Chore> GetAll()
+        {
+            //  We must "use" the database connection.
+            //  Because a database is a shared resource (other applications may be using it too) we must
+            //  be careful about how we interact with it. Specifically, we Open() connections when we need to
+            //  interact with the database and we Close() them when we're finished.
+            //  In C#, a "using" block ensures we correctly disconnect from a resource even if there is an error.
+            //  For database connections, this means the connection will be properly closed.
+            using (SqlConnection conn = Connection)
+            {
+                // Note, we must Open() the connection, the "using" block doesn't do that for us.
+                conn.Open();
+
+                // We must "use" commands too.
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    // Here we setup the command with the SQL we want to execute before we execute it.
+                    cmd.CommandText = "SELECT Id, Name FROM Chore";
+
+                    // Execute the SQL in the database and get a "reader" that will give us access to the data.
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    // A list to hold the chores we retrieve from the database.
+                    List<Chore> chores = new List<Chore>();
+
+                    // Read() will return true if there's more data to read
+                    while (reader.Read())
+                    {
+                        // The "ordinal" is the numeric position of the column in the query results.
+                        //  For our query, "Id" has an ordinal value of 0 and "Name" is 1.
+                        int idColumnPosition = reader.GetOrdinal("Id");
+
+                        // We user the reader's GetXXX methods to get the value for a particular ordinal.
+                        int idValue = reader.GetInt32(idColumnPosition);
+
+                        int nameColumnPosition = reader.GetOrdinal("Name");
+                        string nameValue = reader.GetString(nameColumnPosition);
+
+                        // Now let's create a new chore object using the data from the database.
+                        Chore chore = new Chore
+                        {
+                            Id = idValue,
+                            Name = nameValue,
+                        };
+
+                        // ...and add that chore object to our list.
+                        chores.Add(chore);
+                    }
+
+                    // We should Close() the reader. Unfortunately, a "using" block won't work here.
+                    reader.Close();
+
+                    // Return the list of chores who whomever called this method.
+                    return chores;
+                }
+            }
+        }
+    }
+}
